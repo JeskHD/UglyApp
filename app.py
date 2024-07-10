cb3a01450fc7439592ac8a4fe4153511d7ae7130
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-07-09 23:31:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def downloaded_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Binding to 0.0.0.0 to make the server publicly available
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
