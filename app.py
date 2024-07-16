bb6c517a9c79b662a4a3a67b7183c892e21dbe59
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-07-15 22:15:31 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, request, send_file, render_template_string, redirect, url_for, flash, current_app, send_from_directory, jsonify
+from flask import Flask, request, send_file, render_template_string, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO, emit
 import os
-import base64
-import sqlalchemy as sa
-from flask_sqlalchemy import SQLAlchemy
-from urllib.parse import urlparse
 import requests
 import shutil
-from collections.abc import MutableMapping
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -19,7 +16,7 @@ socketio = SocketIO(app)
 db = SQLAlchemy(app)
 
 # Ensure the downloads directory exists
-DOWNLOADS_DIR = '/opt/render/Downloads'
+DOWNLOADS_DIR = 'static/uploads'
 os.makedirs(DOWNLOADS_DIR, exist_ok=True)
 
 # Example model for demonstration
@@ -27,273 +24,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
 
-# Function to read and encode image files to base64
-def get_base64_image(filepath):
-    with open(filepath, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
-
-# Function to read and encode font files to base64
-def get_base64_font(filepath):
-    with open(filepath, "rb") as font_file:
-        return base64.b64encode(font_file.read()).decode('utf-8')
-
 @app.route('/')
 def index():
     try:
-        background_base64 = get_base64_image('uglygif.gif')
-        logo_base64 = get_base64_image('uglylogo.png')
-        font_base64 = get_base64_font('PORKH___.TTF.ttf')
-
         html_content = '''
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <meta name="HTML WEB DESIGN" content="Web Design">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Ugly Downloader</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <style>
-                @font-face {
-                    font-family: 'Porkys';
-                    src: url(data:font/ttf;base64,{{ font_base64 }}) format('truetype');
-                }
-                * {
-                    box-sizing: border-box;
-                    margin: 0;
-                    padding: 0;
-                }
-                body {
-                    font-family: "Poppins", sans-serif;
-                    width: 100%;
-                    overflow-x: hidden;
-                }
-                .topbar {
-                    font-family: "Montserrat", "Poppins", "Avenir";
-                    width: 100%;
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                    padding: 10px 50px;
-                    background: rgba(0, 0, 0, 0.5);
-                    position: absolute;
-                    top: 1px;
-                    z-index: 1000; /* Ensure topbar is above other content */
-                }
-                .topbar nav {
-                    display: flex;
-                    align-items: center;
-                    width: 100%;
-                }
-                .topbar .menu-toggle {
-                    display: none;
-                    font-size: 24px;
-                    color: white;
-                    cursor: pointer;
-                    position: absolute;
-                    right: 40px;
-                }
-                .message {
-                    position: absolute;
-                    top: 565px;
-                    right: 575px;
-                }
-                .topbar ul {
-                    list-style-type: none;
-                    padding: 0;
-                    margin: 0;
-                    display: flex;
-                    gap: 20px;
-                    position: absolute;
-                    right: 50px;
-                }
-                .topbar ul li {
-                    color: white;
-                }
-                .topbar ul li:hover {
-                    color: rgb(255, 120, 223);
-                    cursor: grab;
-                }
-                .poppins-medium-italic {
-                    font-family: "Poppins", sans-serif;
-                    font-weight: 500;
-                    font-style: italic;
-                }
-                .topbar img {
-                    height: 65px;
-                    width: auto;
-                    position: relative;
-                    top: 2px;
-                }
-                .bimage {
-                    background: linear-gradient(rgba(255, 7, 156, 0.585), rgba(104, 97, 97, 0.5)), url("data:image/gif;base64,{{ background_base64 }}");
-                    height: 800px;
-                    width: 100%;
-                    background-repeat: no-repeat;
-                    background-position: center;
-                    background-size: cover;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    text-align: center;
-                    padding-top: 70px; /* Ensure content is not overlapped by topbar */
-                }
-                .Wrapper {
-                    text-align: center;
-                }
-                .UglyStay {
-                    position: fixed;
-                    top: 225px;
-                    right: 350px;
-                    color: rgb(255, 136, 237);
-                    font-size: 50px;
-                    font-weight: 800;
-                    font-style: italic;
-                }
-                .uglydesc {
-                    position: fixed;
-                    top: 310px;
-                    left: 240px;
-                    color: whitesmoke;
-                }
-                .form-container {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    gap: 10px;
-                    margin-top: 20px;
-                }
-                .searchbox {
-                    width: 300px;
-                    height: 40px;
-                    background-color: black;
-                    border-radius: 50px 0 0 50px;
-                    color: white;
-                    font-family: "Poppins", sans-serif;
-                    text-align: center;
-                    border: none;
-                    padding-left: 20px;
-                }
-                .searchbox:hover {
-                    border: 1px solid #ff78df;
-                }
-                .dropdown1, .dropdown2 {
-                    height: 38px;
-                    border-radius: 0;
-                    padding: 0 9px;
-                    border: none;
-                    font-family: "Poppins", sans-serif;
-                    background-color: #ff78df;
-                    color: white;
-                }
-                .btn1, .btn2 {
-                    height: 38px;
-                    border-radius: 0 50px 50px 0;
-                    padding: 0 7px;
-                    background-color: #fa50d3;
-                    color: white;
-                    border: none;
-                    cursor: pointer;
-                    font-family: "Poppins", sans-serif;
-                }
-                .btn1:active, .btn2:active {
-                    color: #fb85df;
-                    background-color: #f8a1e4;
-                }
-                .btn1:hover, .btn2:hover {
-                    background-color: #e767c7;
-                }
-                .or {
-                    position: relative;
-                    top: 15px;
-                    color: white;
-                }
-                .url {
-                    position: absolute;
-                    top: 540px;
-                    left: 555px;
-                    text-shadow: 0px 3px 5px 0 #c255a7;
-                    color: white;
-                    font-size: 11px;
-                }
-                .sp li:hover {
-                    color: #1d9bf0 !important;
-                }
-                .ua {
-                    font-family: 'Porkys';
-                    color: #f50da1;
-                    font-size: 40px;
-                    text-shadow: 1px 1px 2px #27f1e6;
-                }
-                .flashes {
-                    color: red;
-                    list-style: none;
-                    text-align: center;
-                    margin-top: 10px;
-                }
-                /* Responsive Design */
-                @media (max-width: 800px) {
-                    .topbar {
-                        flex-direction: row;
-                        align-items: center;
-                        padding: 10px 10px;
-                    }
-                    .topbar .menu-toggle {
-                        display: block;
-                    }
-                    .topbar ul {
-                        display: none;
-                        flex-direction: column;
-                        align-items: center;
-                        width: 100%;
-                        margin-top: 10px;
-                    }
-                    .topbar ul.active {
-                        display: flex;
-                        font-size: 10px;
-                        top: 11px;
-                        border: 1px solid white;
-                        flex-direction: column;
-                        position: absolute;
-                        background-color: rgba(0, 0, 0, 0.8);
-                        right: 10px;
-                        top: 60px;
-                        width: 200px;
-                        padding: 10px;
-                    }
-                    .topbar h2 {
-                        font-size: 24px;
-                    }
-                    .UglyStay {
-                        font-size: 30px;
-                        top: 110px;
-                        right: 40px;
-                    }
-                    .uglydesc {
-                        position: absolute;
-                        top: 200px;
-                        left: 10px;
-                        right: 10px;
-                        font-size: 14px;
-                    }
-                    .form-container {
-                        flex-direction: column;
-                        align-items: center;
-                    }
-                    .searchbox, .dropdown1, .dropdown2, .btn1, .btn2 {
-                        width: 100%;
-                        margin-bottom: 10px;
-                        position: relative;
-                    }
-                    .url {
-                        top: 650px;
-                        left: 50%;
-                        transform: translateX(-50%);
-                    }
-                }
+                /* CSS styles here */
             </style>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.min.js"></script>
             <script>
@@ -337,7 +79,7 @@ def index():
                                 <p class="uglydesc">Download Ugly Bros' art, music, and videos swiftly with UglyDownloader. Quality and simplicity in one click.</p>
                                 <br>
                                 <div class="form-container">
-                                    <form action="/download" method="post" enctype="multipart/form-data">
+                                    <form action="/download" method="post">
                                         <div class="AllC">
                                             <input type="text" name="audio_url" placeholder="Enter audio URL" class="searchbox">
                                             <select name="audio_format" class="dropdown1">
@@ -376,7 +118,7 @@ def index():
         </body>
         </html>
         '''
-        return render_template_string(html_content, background_base64=background_base64, font_base64=font_base64)
+        return render_template_string(html_content)
     except Exception as e:
         return f"Error rendering page: {str(e)}"
 
@@ -396,13 +138,18 @@ def download():
         return redirect(url_for('index'))
 
     try:
-        return handle_direct_download(url, format, request.form)
+        return handle_direct_download(url, format)
     except requests.exceptions.RequestException as e:
         flash(f"Error: {str(e)}")
         return redirect(url_for('index'))
 
-def handle_direct_download(url, format, form):
+def handle_direct_download(url, format):
     filename = url.split('/')[-1]
+    if format == 'audio':
+        filename += '.mp3'
+    else:
+        filename += '.mp4'
+
     filepath = os.path.join(DOWNLOADS_DIR, filename)
 
     with requests.get(url, stream=True) as r:
@@ -419,22 +166,6 @@ def send_file_response(file_to_send):
     else:
         flash("File not found after download.")
         return redirect(url_for('index'))
-
-@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
-def upload(filename):
-    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
-    return send_from_directory(uploads, filename)
-
-# Database initialization logic for Render
-engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspector = sa.inspect(engine)
-if not inspector.has_table("user"):
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        app.logger.info('Initialized the database!')
-else:
-    app.logger.info('Database already contains the users table.')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
