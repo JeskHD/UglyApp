d2c0aa5732ff470f125391568bcdf5a7fc7f452b
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-07-17 00:53:35 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def index():
                     height: 65px;
                     width: auto;
                     position: relative;
-                    top: 2px;
+                    top: 2px.
                 }
                 .bimage {
                     background: linear-gradient(rgba(255, 7, 156, 0.585), rgba(104, 97, 97, 0.5)), url("data:image/gif;base64,{{ background_base64 }}");
@@ -133,7 +133,7 @@ def index():
                 }
                 .Wrapper {
                     text-align: center;
-                    padding: 20px;
+                    padding: 20px.
                 }
                 .UglyStay {
                     color: rgb(255, 136, 237);
@@ -142,14 +142,14 @@ def index():
                     font-style: italic;
                     margin: 0 20px;
                     text-align: center;
-                    width: 100%;
+                    width: 100%.
                 }
                 .uglydesc {
                     color: whitesmoke;
                     margin: 20px 10px;
                     font-size: 18px;
                     text-align: center;
-                    width: 100%;
+                    width: 100%.
                 }
                 .form-container {
                     display: flex;
@@ -157,7 +157,7 @@ def index():
                     justify-content: center;
                     gap: 10px;
                     margin-top: 20px;
-                    flex-wrap: wrap;
+                    flex-wrap: wrap.
                 }
                 .searchbox {
                     width: 300px;
@@ -168,10 +168,10 @@ def index():
                     font-family: "Poppins", sans-serif;
                     text-align: center;
                     border: none;
-                    padding-left: 20px;
+                    padding-left: 20px.
                 }
                 .searchbox:hover {
-                    border: 1px solid #ff78df;
+                    border: 1px solid #ff78df.
                 }
                 .dropdown1, .dropdown2 {
                     height: 38px;
@@ -180,7 +180,7 @@ def index():
                     border: none;
                     font-family: "Poppins", sans-serif;
                     background-color: #ff78df;
-                    color: white;
+                    color: white.
                 }
                 .btn1, .btn2 {
                     height: 38px;
@@ -190,21 +190,21 @@ def index():
                     color: white;
                     border: none;
                     cursor: pointer;
-                    font-family: "Poppins", sans-serif;
+                    font-family: "Poppins", sans-serif.
                 }
                 .btn1:active, .btn2:active {
                     color: #fb85df;
-                    background-color: #f8a1e4;
+                    background-color: #f8a1e4.
                 }
                 .btn1:hover, .btn2:hover {
-                    background-color: #e767c7;
+                    background-color: #e767c7.
                 }
                 .or {
                     position: relative;
                     top: 15px;
                     color: white;
                     font-size: 18px;
-                    margin: 10px 0;
+                    margin: 10px 0.
                 }
                 .url {
                     text-shadow: 0px 3px 5px 0 #c255a7;
@@ -212,39 +212,39 @@ def index():
                     font-size: 14px;
                     margin-top: 10px;
                     width: 100%;
-                    text-align: center;
+                    text-align: center.
                 }
                 .sp li:hover {
-                    color: #1d9bf0 !important;
+                    color: #1d9bf0 !important.
                 }
                 .ua {
                     font-family: 'Porkys';
                     color: #f50da1;
                     font-size: 40px;
-                    text-shadow: 1px 1px 2px #27f1e6;
+                    text-shadow: 1px 1px 2px #27f1e6.
                 }
                 .flashes {
                     color: red;
                     list-style: none;
                     text-align: center;
-                    margin-top: 10px;
+                    margin-top: 10px.
                 }
                 /* Responsive Design */
                 @media (max-width: 800px) {
                     .topbar {
                         flex-direction: row;
                         align-items: center;
-                        padding: 10px 10px;
+                        padding: 10px 10px.
                     }
                     .topbar .menu-toggle {
-                        display: block;
+                        display: block.
                     }
                     .topbar ul {
                         display: none;
                         flex-direction: column;
                         align-items: center;
                         width: 100%;
-                        margin-top: 10px;
+                        margin-top: 10px.
                     }
                     .topbar ul.active {
                         display: flex;
@@ -257,36 +257,36 @@ def index():
                         right: 10px;
                         top: 60px;
                         width: 200px;
-                        padding: 10px;
+                        padding: 10px.
                     }
                     .topbar h2 {
-                        font-size: 24px;
+                        font-size: 24px.
                     }
                     .UglyStay {
                         font-size: 30px;
                         margin-top: 80px;
-                        text-align: center;
+                        text-align: center.
                     }
                     .uglydesc {
                         font-size: 16px;
                         margin: 20px 20px;
-                        text-align: center;
+                        text-align: center.
                     }
                     .form-container {
                         flex-direction: column;
-                        align-items: center;
+                        align-items: center.
                     }
                     .searchbox, .dropdown1, .dropdown2, .btn1, .btn2 {
                         width: 100%;
-                        margin-bottom: 10px;
+                        margin-bottom: 10px.
                     }
                     .or {
                         top: 0;
-                        margin: 10px 0;
+                        margin: 10px 0.
                     }
                     .url {
                         margin-top: 20px;
-                        text-align: center;
+                        text-align: center.
                     }
                 }
             </style>
@@ -369,7 +369,7 @@ def index():
                 </main>
             </div>
         </body>
-        </html>        
+        </html>
         '''
         return render_template_string(html_content, background_base64=background_base64, font_base64=font_base64)
     except Exception as e:
@@ -388,8 +388,8 @@ def download():
 
     try:
         # Paths to ffmpeg and ffprobe
-        ffmpeg_location = '/ffmpeg/bin/ffmpeg'
-        ffprobe_location = '/ffmpeg/bin/ffprobe'
+        ffmpeg_location = '/usr/bin/ffmpeg'
+        ffprobe_location = '/usr/bin/ffprobe'
 
         ydl_opts = {
             'outtmpl': os.path.join(DOWNLOADS_DIR, '%(title)s.%(ext)s'),
