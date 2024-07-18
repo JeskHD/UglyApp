fe063575c716380ba357eee341dde3caa625feb5
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-07-17 22:43:17 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ def download():
 
         if "twitter.com/i/spaces" in url or "x.com/i/spaces" in url:
             cookie_file = 'cookies_netscape.txt'
-            output_template = os.path.join(DOWNLOADS_DIR, 'downloaded_file')
+            output_template = os.path.join(DOWNLOADS_DIR, '%(title)s')
             command = [
                 'twspace_dl',
                 '-i', url,
