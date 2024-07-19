4206d8ba6cae3ae345100edd30097f835360bc9b
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-07-19 00:04:40 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def index():
         font_base64 = get_base64_font('PORKH___.TTF.ttf')
 
         html_content = '''
-     <!DOCTYPE html>
+       <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -423,15 +423,15 @@ def download():
         ffmpeg_location = '/usr/bin/ffmpeg'
         ffprobe_location = '/usr/bin/ffprobe'
 
-        cookie_file = None
         ydl_opts = {
             'outtmpl': os.path.join(DOWNLOADS_DIR, '%(title)s.%(ext)s'),
             'ffmpeg_location': ffmpeg_location,
             'ffprobe_location': ffprobe_location,
-            'hls_use_mpegts': True,  # Ensure HLS processing for all formats
+            'hls_use_mpegts': True  # Ensure HLS processing for all formats
         }
 
         if "twitter.com/i/spaces" in url or "x.com/i/spaces" in url:
+            logger.debug("Downloading from Twitter Spaces")
             cookie_file = 'cookies_netscape.txt'
             audio_format = request.form.get('audio_format', 'm4a/mp3')
             output_template = os.path.join(DOWNLOADS_DIR, '%(title)s')
@@ -450,7 +450,7 @@ def download():
                 if process.poll() is not None:
                     break
                 if output:
-                    print(output.strip())
+                    logger.debug(output.strip())
                     socketio.emit('eta', {'data': output.strip()})
 
             process.wait()
@@ -469,7 +469,8 @@ def download():
                             '-i', latest_file,
                             '-codec:a', 'libmp3lame',
                             '-qscale:a', '2',
-                            mp3_file
+                            mp3_file,
+                            '-n'  # Do not overwrite existing files
                         ]
                         subprocess.run(convert_command, check=True)
                         latest_file = mp3_file
@@ -482,81 +483,70 @@ def download():
             else:
                 flash("Error during the download process.")
                 return redirect(url_for('index'))
-        
-        elif 'youtube.com' in url:
-            cookie_file = 'youtube_cookies.txt'
-            ydl_opts.update({
-                'cookiefile': cookie_file,
-                'username': 'oauth2',
-                'password': '',
-            })
-
-        elif 'soundcloud.com' in url:
-            cookie_file = 'soundcloud_cookies.txt'
-            ydl_opts.update({
-                'cookiefile': cookie_file,
-            })
-        
-        if format == 'audio':
-            audio_format = request.form['audio_format']
-            ydl_opts.update({
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': audio_format,
-                    'preferredquality': '192',
-                }]
-            })
         else:
-            video_format = request.form['video_format']
-            ydl_opts.update({
-                'format': 'bestvideo+bestaudio/best',
-                'merge_output_format': 'mp4'
-            })
-
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=True)
-            file_path = ydl.prepare_filename(info_dict)
-
             if format == 'audio':
-                file_path = file_path.replace('.webm', f'.{audio_format}').replace('.opus', f'.{audio_format}')
+                audio_format = request.form['audio_format']
+                ydl_opts.update({
+                    'format': 'bestaudio/best',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': audio_format,
+                        'preferredquality': '192',
+                    }]
+                })
             else:
-                if video_format == 'mov':
-                    file_path = file_path.replace('.mp4', f'.mp4')
-                else:
-                    file_path = file_path.replace('.mp4', f'.{video_format}').replace('.m4a', f'.{video_format}')
-                
-            if os.path.exists(file_path):
-                if format == 'audio' and audio_format == 'mp3':
-                    mp3_file = file_path.replace('.m4a', '.mp3')
-                    convert_command = [
-                        ffmpeg_location,
-                        '-i', file_path,
-                        '-codec:a', 'libmp3lame',
-                        '-qscale:a', '2',
-                        mp3_file
-                    ]
-                    subprocess.run(convert_command, check=True)
-                    file_to_send = mp3_file
-                elif format == 'video' and video_format == 'mov':
-                    mov_file = file_path.replace('.mp4', '.mov')
-                    convert_command = [
-                        ffmpeg_location,
-                        '-i', file_path,
-                        '-c:v', 'copy',
-                        '-c:a', 'copy',
-                        mov_file
-                    ]
-                    subprocess.run(convert_command, check=True)
-                    file_to_send = mov_file
-                else:
-                    file_to_send = file_path
+                video_format = request.form['video_format']
+                ydl_opts.update({
+                    'format': 'bestvideo+bestaudio/best',
+                    'merge_output_format': 'mp4'
+                })
 
-                socketio.emit('download_complete', {'filename': os.path.basename(file_to_send)})
-                return send_file(file_to_send, as_attachment=True, download_name=os.path.basename(file_to_send))
-            else:
-                flash("File not found after download.")
-                return redirect(url_for('index'))
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                logger.debug("Starting download with yt-dlp")
+                info_dict = ydl.extract_info(url, download=True)
+                file_path = ydl.prepare_filename(info_dict)
+
+                if format == 'audio':
+                    file_path = file_path.replace('.webm', f'.{audio_format}').replace('.opus', f'.{audio_format}')
+                else:
+                    if video_format == 'mov':
+                        file_path = file_path.replace('.mp4', f'.mp4')
+                    else:
+                        file_path = file_path.replace('.mp4', f'.{video_format}').replace('.m4a', f'.{video_format}')
+
+                if os.path.exists(file_path):
+                    if format == 'audio' and audio_format == 'mp3':
+                        mp3_file = file_path.replace('.m4a', '.mp3')
+                        convert_command = [
+                            ffmpeg_location,
+                            '-i', file_path,
+                            '-codec:a', 'libmp3lame',
+                            '-qscale:a', '2',
+                            mp3_file,
+                            '-n'  # Do not overwrite existing files
+                        ]
+                        subprocess.run(convert_command, check=True)
+                        file_to_send = mp3_file
+                    elif format == 'video' and video_format == 'mov':
+                        mov_file = file_path.replace('.mp4', '.mov')
+                        convert_command = [
+                            ffmpeg_location,
+                            '-i', file_path,
+                            '-c:v', 'copy',
+                            '-c:a', 'copy',
+                            mov_file,
+                            '-n'  # Do not overwrite existing files
+                        ]
+                        subprocess.run(convert_command, check=True)
+                        file_to_send = mov_file
+                    else:
+                        file_to_send = file_path
+
+                    socketio.emit('download_complete', {'filename': os.path.basename(file_to_send)})
+                    return send_file(file_to_send, as_attachment=True, download_name=os.path.basename(file_to_send))
+                else:
+                    flash("File not found after download.")
+                    return redirect(url_for('index'))
     except subprocess.CalledProcessError as e:
         flash(f"Error: {str(e)}")
         return redirect(url_for('index'))
