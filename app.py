5e13d477c64702f74ea2e24d881d133b6793ca2e
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-07-20 18:43:13 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -418,7 +418,7 @@ def download():
 
             command = [
                 '/root/UglyApp/venv/bin/python3', '-m', 'twspace_dl',
-                '-c', '/usr/cookies_netscape.txt',  # Use the cookies file uploaded
+                '-c', '/root/UglyApp/cookies_netscape.txt',  # Use the cookies file uploaded
                 '-i', url
             ]
 
