900ecb4041bc2c0e27aa06a91bdbd56c431eb9a3
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-08-13 11:59:59 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -262,8 +262,8 @@ def index():
             width: 300px;
             margin: 0 auto; /* Center the progress bar */
             display: none; /* Hidden by default */
-            position: fixed;
-            top: 90%; /* Adjust to be fixed near the bottom of the page */
+            position: relative;
+            top: 95%; /* Adjust to be fixed near the bottom of the page */
             left: 50%;
             transform: translateX(-50%);
             z-index: 2000; /* Ensure it stays above other content */
