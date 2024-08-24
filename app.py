1f784b2345e6187c942b2caa7bb5d21db4ead55f
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-08-24 14:10:27 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_base64_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 def get_base64_font(font_path):
-    with open(font_path, "rb") as font_file):
+    with open(font_path, "rb") as font_file:
         return base64.b64encode(font_file.read()).decode('utf-8')
 
 @app.route('/')
