2e0fa129f4c46ff06a76c4215fa362113537c285
JeskHD <147014336+JeskHD@users.noreply.github.com>
2024-08-24 14:23:04 -0400

    Update app.py
    Signed-off-by: JeskHD <147014336+JeskHD@users.noreply.github.com>

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def index():
             <meta name="HTML WEB DESIGN" content="Web Design">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Ugly Downloader</title>
-            <link rel="icon" href="{{ url_for('favicon') }}" type="image/x-icon">
+            <link rel="icon" href="favico.ico" type="image/x-icon">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
